d20f0738e6e84fb8eee279723b60982ec509a51b
Alessandro Falappa <alex.falappa@gmail.com>
2019-01-23 20:33:25 +0100

    Complete UI Revamp (#6)
    * Initial attempt at a revamped GUI.
    
    * Continued in UI revamp.
    
    Different layout, regex history, match and group highlighting, separate
    regex and replacement errors, throttled updates.
    
    * Introduced Ikonli dependency, used icon on save button.
    
    * Deleted debug messages and edit listeners
    
    * Added load/save of state from/to properties
    
    * Renamed top component back to original name and fixed license headers.
    
    * Fixed generated bundle method name
    
    * Added plugin preferences in NetBeans options dialog.
    
    Store highlighting colors.
    
    * Tweaked highlight colors
    
    * Changed layout of options panel
    
    * Added listening to options changes
    
    * Layout tweak for the groups text pane
    
    * Rethinked settings.
    
    No more panel in NetBeans options but simple dialog.
    
    * Fix group colors different in text and groups panes
    
    * Added dark palette and buttons to set palettes
    
    * Better icon creation logic

--- NetbeansRegexPlugin/src/main/java/de/dev/eth0/netbeans/plugins/regex/RegexEvaluator.java
+++ NetbeansRegexPlugin/src/main/java/de/dev/eth0/netbeans/plugins/regex/RegexEvaluator.java
@@ -1,9 +1,7 @@
 /*
- * #%L
- * dev-eth0.de
- * %%
  * Copyright (C) 2016 dev-eth0.de
- * %%
+ * Modifications copyright (C) 2018 Alessandro Falappa.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
 package de.dev.eth0.netbeans.plugins.regex;
 
@@ -24,100 +21,110 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 /**
+ * Evaluates a regular expression string keeping track of validation errors.
  *
  * @author deveth0
+ * @author Alessandro Falappa
  */
 public class RegexEvaluator {
 
-  private String regex = "";
-  private int flags = 0;
-  private Pattern pattern;
+    private String regex = "";
+    private int flags = 0;
+    private Pattern pattern;
 
-  private Matcher matcher;
+    private Matcher matcher;
 
-  public boolean valid;
-  public String invalidReason;
-  public boolean matches;
-  public String replaceFirst;
-  public String replaceAll;
-  public boolean lookingAt;
-  public boolean find;
-  public MatcherGroup[] groups = new MatcherGroup[0];
-
-  /**
-   * Update the evaluator with new input
-   *
-   * @param regex
-   * @param replacement
-   * @param input
-   */
-  public void update(String regex, int flags, String replacement, String input) {
-    try {
-      //TODO: catch empty/null exceptions
-      if (!this.regex.equals(regex) || this.flags != flags) {
-        pattern = Pattern.compile(regex, flags);
-        this.regex = regex;
-        this.flags = flags;
-        this.valid = true;
-      }
-      if (pattern != null) {
-        matcher = pattern.matcher(input);
-        matches = matcher.matches();
-        matcher.reset();
-        if (isNotEmpty(replacement)) {
-          try {
-            replaceFirst = matcher.replaceFirst(replacement);
-            replaceAll = matcher.replaceAll(replacement);
-          }
-          catch (IllegalArgumentException iae) {
-            this.valid = false;
-            this.invalidReason = iae.getMessage();
-          }
-        }
-        lookingAt = matcher.lookingAt();
-        matcher.reset();
-        find = matcher.find();
-        if (find) {
-          groups = new MatcherGroup[matcher.groupCount() + 1];
-          for (int i = 0; i < matcher.groupCount() + 1; i++) {
-            groups[i] = new MatcherGroup(matcher.start(i), matcher.end(i), matcher.group(i));
-          }
-        }
-        else {
-          groups = new MatcherGroup[0];
-        }
-      }
-    }
-    catch (PatternSyntaxException pse) {
-      this.valid = false;
-      this.invalidReason = pse.getMessage();
-    }
-  }
-
-  private boolean isNotEmpty(String toTest) {
-    return toTest != null && !toTest.isEmpty();
-  }
-
-  /**
-   * A single matcher group
-   */
-  public static class MatcherGroup {
-
-    public final int start;
-    public final int end;
-    public final String group;
+    public boolean validRegex;
+    public boolean validReplex;
+    public String regexReason;
+    public String replexReason;
+    public boolean matches;
+    public String replaceFirst;
+    public String replaceAll;
+    public boolean lookingAt;
+    public boolean find;
+    public MatcherGroup[] groups = new MatcherGroup[0];
 
     /**
+     * Update the evaluator with new input
      *
-     * @param start
-     * @param end
-     * @param group
+     * @param regex
+     * @param replacement
+     * @param input
      */
-    public MatcherGroup(int start, int end, String group) {
-      this.start = start;
-      this.end = end;
-      this.group = group;
+    public void update(String regex, int flags, String replacement, String input) {
+        try {
+            //TODO: catch empty/null exceptions
+            if (!this.regex.equals(regex) || this.flags != flags) {
+                pattern = Pattern.compile(regex, flags);
+                this.regex = regex;
+                this.flags = flags;
+                this.validRegex = true;
+                this.validReplex = true;
+            }
+            if (pattern != null) {
+                matcher = pattern.matcher(input);
+                matches = matcher.matches();
+                matcher.reset();
+                if (isNotEmpty(replacement)) {
+                    try {
+                        replaceFirst = matcher.replaceFirst(replacement);
+                        replaceAll = matcher.replaceAll(replacement);
+                        validReplex = true;
+                    } catch (IllegalArgumentException iae) {
+                        validReplex = false;
+                        replexReason = iae.getMessage();
+                        replaceAll = "";
+                        replaceFirst = "";
+                    } catch (IndexOutOfBoundsException ioe) {
+                        validReplex = false;
+                        replexReason = ioe.getMessage();
+                        replaceAll = "";
+                        replaceFirst = "";
+                    }
+                }
+                lookingAt = matcher.lookingAt();
+                matcher.reset();
+                find = matcher.find();
+                if (find) {
+                    groups = new MatcherGroup[matcher.groupCount() + 1];
+                    for (int i = 0; i < matcher.groupCount() + 1; i++) {
+                        groups[i] = new MatcherGroup(matcher.start(i), matcher.end(i), matcher.group(i));
+                    }
+                } else {
+                    groups = new MatcherGroup[0];
+                }
+            }
+        } catch (PatternSyntaxException pse) {
+            this.validRegex = false;
+            this.regexReason = pse.getMessage();
+        }
     }
 
-  }
+    private boolean isNotEmpty(String toTest) {
+        return toTest != null && !toTest.isEmpty();
+    }
+
+    /**
+     * A single matcher group
+     */
+    public static class MatcherGroup {
+
+        public final int start;
+        public final int end;
+        public final String group;
+
+        /**
+         *
+         * @param start
+         * @param end
+         * @param group
+         */
+        public MatcherGroup(int start, int end, String group) {
+            this.start = start;
+            this.end = end;
+            this.group = group;
+        }
+
+    }
 }
